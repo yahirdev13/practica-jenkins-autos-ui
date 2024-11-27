74b1e8f1ab48ca89cd1c4cead0a47d8c76ab117b
Yahir Alberto <136031489+yahirdev13@users.noreply.github.com>
2024-11-26 23:01:47 -0600

    Update Home.vue: add third test header for demonstration purposes

--- gpds-ui/src/views/Home.vue
+++ gpds-ui/src/views/Home.vue
@@ -4,6 +4,7 @@
     <h1>Home</h1>
     <h1>Cambio de prueba xd</h1>
     <h1>Cambio de prueba 2.0</h1>
+    <h1>Cambio de prueba 3.0</h1>
     <p>Bienvenido a la página de inicio.</p>
   </div>
 </template>
