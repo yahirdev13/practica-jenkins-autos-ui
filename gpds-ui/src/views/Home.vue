c5cf71acfe71a926e0fdd5861079cac017f4e574
Yahir Alberto <136031489+yahirdev13@users.noreply.github.com>
2024-11-27 13:12:06 -0600

    ultimo cambio

--- gpds-ui/src/views/Home.vue
+++ gpds-ui/src/views/Home.vue
@@ -5,6 +5,7 @@
     <h1>Cambio de prueba xd</h1>
     <h1>Cambio de prueba 2.0</h1>
     <h1>Cambio de prueba 3.0</h1>
+    <h1>Cambio de prueba 4.0 y final</h1>
     <p>Bienvenido a la página de inicio.</p>
   </div>
 </template>
